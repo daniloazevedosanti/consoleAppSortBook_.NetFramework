6e5133105441125e6eeadcaf3bc0236f8f9cde66
Danilo Azevedo Santos <desenvolvedor12@carrera.com.br>
2023-08-15 11:33:51 -0300

    commit sort by books 76%

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleAppSortBooks.entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,25 @@ namespace ConsoleAppSortBooks
     {
         static void Main(string[] args)
         {
+            bool option = false;
+            List<Book> books = new List<Book>();
+
+            while (!option)
+            {
+                Console.WriteLine("Insira dados do livro;");
+                Console.WriteLine("Titulo:"); 
+                string titulo = Console.ReadLine();
+                Console.WriteLine("Autor:");
+                string autor = Console.ReadLine();
+                Console.WriteLine("Data Lançamento (dd-MM-yyyy):");
+                string dataLancamento = Console.ReadLine();
+
+                Book book = new Book() 
+                { 
+                
+                };
+
+            }
         }
     }
 }
---
+++ entities/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppSortBooks.entities
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+    }
+}
